ec96ab88675ef79ba8438a7c6dc045734bbd7e08
Harshavardhan_19 <140223982+HarshavardhanKakarla@users.noreply.github.com>
2024-07-28 16:06:38 +0530

    Add files via upload

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBar.css'
+
+
+const SearchBar = () => {
+  return (
+    <div className='Search'>
+        <h2>Search</h2>
+      
+    </div>
+  )
+}
+
+export default SearchBar
